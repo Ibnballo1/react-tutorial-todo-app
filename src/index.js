2d2c1214ca42736de9f4c9533ec3059f2e71cae7
Ibnballo1 <belloabdullateef035@gmail.com>
2022-09-13 08:46:56 +0100

    Refactor index.js to use createRoot

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import TodoContainer from "./components/TodoContainer";
 
-ReactDOM.render(
-<React.StrictMode>
-  <TodoContainer />
-</React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>
+)
